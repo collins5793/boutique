68b566e5188b2ab47d59943281fdeca6d8f0ffa5
collins5793 <collinstossou197@gmail.com>
2025-08-12 17:22:32 +0100

    migration et breeze

---
+++ database/migrations/2025_08_12_154811_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 191);
+            $table->string('phone', 20);
+            $table->string('address_line', 191);
+            $table->string('city', 100);
+            $table->string('country', 100);
+            $table->string('postal_code', 20)->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
